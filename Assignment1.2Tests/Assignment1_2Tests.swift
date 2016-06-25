683b2836e07ac76d56989db94df6b887e66a0e1f
Michael Zhou <michaelzhou@michaels-macbook-pro.local>
2016-06-24 22:14:11 -0400

    Initial Commit

---
+++ Assignment1.2Tests/Assignment1_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Assignment1_2Tests.swift
+//  Assignment1.2Tests
+//
+//  Created by Michael Zhou on 6/24/16.
+//  Copyright © 2016 Michael Zhou. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment1_2
+
+class Assignment1_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
